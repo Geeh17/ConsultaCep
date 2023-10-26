1cd43ffbe2d6dde823ee4b1867be4f11ac651056
113297903+Geeh17@users.noreply.github.com <141880337+GldaSilva17@users.noreply.github.com>
2023-10-26 00:05:51 -0300

    atualização do css e do html na busca do cep

--- Js/index.js
+++ Js/index.js
@@ -1,24 +1,31 @@
 document.getElementById("buscarCEP").addEventListener("click", function () {
-    const cep = document.getElementById("cepInput").value;
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    const cepInput = document.getElementById("cepInput");
+    const cep = cepInput.value;
+    cepInput.value = cep.replace(/\D/g, '');
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.erro) {
-                document.getElementById("resultado").textContent = "CEP não encontrado";
-            } else {
-                document.getElementById("resultado").innerHTML = `
-                    <p>CEP: ${data.cep}</p>
-                    <p>Logradouro: ${data.logradouro}</p>
-                    <p>Bairro: ${data.bairro}</p>
-                    <p>Cidade: ${data.localidade}</p>
-                    <p>Estado: ${data.uf}</p>
-                `;
-            }
-        })
-        .catch(error => {
-            console.error("Ocorreu um erro ao buscar o CEP:", error);
-            document.getElementById("resultado").textContent = "Ocorreu um erro ao buscar o CEP";
-        });
+    if (cep.length !== 8) {
+        document.getElementById("resultado").textContent = "CEP invalido. Digite apenas numeros.";
+    } else {
+        const url = `https://viacep.com.br/ws/${cep}/json`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.erro) {
+                    document.getElementById("resultado").textContent = "CEP não encontrado";
+                } else {
+                    document.getElementById("resultado").innerHTML = `
+                        <p>CEP: ${data.cep}</p>
+                        <p>Logradouro: ${data.logradouro}</p>
+                        <p>Bairro: ${data.bairro}</p>
+                        <p>Cidade: ${data.localidade}</p>
+                        <p>Estado: ${data.uf}</p>
+                    `;
+                }
+            })
+            .catch(error => {
+                console.error("Ocorreu um erro ao buscar o CEP:", error);
+                document.getElementById("resultado").textContent = "Ocorreu um erro ao buscar o CEP";
+            });
+    }
 });
